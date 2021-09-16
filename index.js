89449bfdf0b6ceb9c5927fbc3c450a2835e6771f
Ahmed Afridee <a.afridee47@gmail.com>
2021-09-16 23:42:34 +0600

    ...

--- index.js
+++ index.js
@@ -5,13 +5,8 @@ const app = express();
 require('./startup/routes')(app);
 require('./startup/prod')(app);
 
-<<<<<<< HEAD
-const port = process.env.PORT || 3000;
-
-=======
 
 const port = process.env.PORT || 3000;
->>>>>>> 64d77a04624dba74d6a2413407045838e7a29d56
 const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 
